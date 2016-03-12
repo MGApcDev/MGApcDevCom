40e41d3fa763d6f39deb7b21bf55fac8852275de
MGApcDev <mgapcdev@gmail.com>
2016-03-12 06:57:53 -0700

    Add script for concat js files and yui compressor

---
+++ lib/jsconcat/jsconcat.php
@@ -0,0 +1,22 @@
+<?php
+$scriptPath = '/home4/mgapcdev/public_html/themes/custom/mgapcdev/scripts/';
+
+$scripts = array(
+  'vendor/github-activity-0.1.4.min.js',
+  'script.js',
+);
+
+$file = fopen($scriptPath . 'build/main.js', 'w');
+foreach ($scripts as $script) {
+  $path = $scriptPath . $script;
+  if (file_exists($path)) {
+    fwrite($file, file_get_contents($path));
+  } else {
+    echo 'File not found --> ' . $path . PHP_EOL;
+    fclose($file);
+    die();
+  }
+}
+fclose($file);
+echo 'JS compiled --> success ' . PHP_EOL;
+?>
